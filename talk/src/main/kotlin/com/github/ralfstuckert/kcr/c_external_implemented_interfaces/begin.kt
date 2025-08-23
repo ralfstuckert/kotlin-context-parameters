f4db717f886341112c55b0b5058dd8c97e456b7e
Ralf Stuckert <ralf.stuckert@compeople.de>
2025-08-23 14:19:40 +0200

    add talk

--- talk/src/main/kotlin/com/github/ralfstuckert/kcr/c_external_implemented_interfaces/begin.kt
+++ talk/src/main/kotlin/com/github/ralfstuckert/kcr/c_external_implemented_interfaces/begin.kt
@@ -6,10 +6,6 @@ import com.github.ralfstuckert.kcr.b_scoped_functions.User
 fun <T : Comparable<T>> max(x: T, y: T) =
     if (x > y) x else y
 
-// region generic max
-// fun <T> max(x: T, y: T) = if (x > y) x else y
-// endregion
-
 
 fun main() {
 
